1e07a3c68bb6356ac5c03f2e7c7085794a674410
itaminul@gmail.com <itaminul@gmail.com>
2024-05-26 18:43:30 +0600

    create doctor controller

--- src/doctor-information/doctor-information.controller.ts
+++ src/doctor-information/doctor-information.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { DoctorInformationService } from './doctor-information.service';
+import { CreateDoctorInformationDto } from './dto/create.doctor.information.dto';
 
 @Controller('doctor-information')
-export class DoctorInformationController {}
+export class DoctorInformationController {
+    constructor(public readonly doctorInformationService: DoctorInformationService){}
+    @Post()
+    async create(@Body() dto: CreateDoctorInformationDto) {
+        try {
+            const results = await this.doctorInformationService.create(dto)           
+            return {
+                message: 'Show Successfully',
+                success: true,
+                status: HttpStatus.OK,
+                results,
+              };
+            } catch (error) {
+              return { success: false, message: error.message };
+            }
+    }
+}
